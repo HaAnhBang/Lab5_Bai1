99d1e4157f7b2f213443e50c2637de73472a1539
HaAnhBang <haanhbang4427@gmail.com>
2026-05-19 16:32:15 +0700

    Update NhanVienSX.cpp

--- NhanVienSX.cpp
+++ NhanVienSX.cpp
@@ -1,1 +1,37 @@
 #include "NhanVienSX.h"
+
+NhanVienSX::NhanVienSX()
+{
+    LuongCanBan = 0;
+    SoSanPham = 0;
+}
+
+NhanVienSX::~NhanVienSX()
+{
+}
+
+void NhanVienSX::Nhap()
+{
+    NhanVien::Nhap();
+
+    cout << "Nhap luong can ban: ";
+    cin >> LuongCanBan;
+
+    cout << "Nhap so san pham: ";
+    cin >> SoSanPham;
+
+    TinhLuong();
+}
+
+void NhanVienSX::Xuat()
+{
+    NhanVien::Xuat();
+
+    cout << "Luong can ban: " << LuongCanBan << endl;
+    cout << "So san pham: " << SoSanPham << endl;
+}
+
+void NhanVienSX::TinhLuong()
+{
+    Luong = LuongCanBan + SoSanPham * 5000;
+}
