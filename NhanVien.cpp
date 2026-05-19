240ef3144b54413c9bd62646cb0e46795603a79e
HaAnhBang <haanhbang4427@gmail.com>
2026-05-19 16:32:42 +0700

    Update NhanVien.cpp

--- NhanVien.cpp
+++ NhanVien.cpp
@@ -17,13 +17,20 @@ void NhanVien::Nhap()
     getline(cin, HoTen);
 
     cout << "Nhap ngay sinh: ";
-    cin >> ngay >> thang >> nam;
+    cin >> ngay;
+
+    cout << "Nhap thang sinh: ";
+    cin >> thang;
+
+    cout << "Nhap nam sinh: ";
+    cin >> nam;
 }
 
 void NhanVien::Xuat()
 {
     cout << "Ho ten: " << HoTen << endl;
-    cout << "Ngay sinh: " << ngay << "/" << thang << "/" << nam << endl;
+    cout << "Ngay sinh: "
+         << ngay << "/" << thang << "/" << nam << endl;
     cout << "Luong: " << Luong << endl;
 }
 
@@ -35,75 +42,4 @@ long long NhanVien::GetLuong()
 int NhanVien::GetNamSinh()
 {
     return nam;
-}
-
-// ================= NHAN VIEN SAN XUAT =================
-
-NhanVienSX::NhanVienSX()
-{
-    LuongCanBan = 0;
-    SoSanPham = 0;
-}
-
-NhanVienSX::~NhanVienSX()
-{
-}
-
-void NhanVienSX::Nhap()
-{
-    NhanVien::Nhap();
-
-    cout << "Nhap luong can ban: ";
-    cin >> LuongCanBan;
-
-    cout << "Nhap so san pham: ";
-    cin >> SoSanPham;
-
-    TinhLuong();
-}
-
-void NhanVienSX::TinhLuong()
-{
-    Luong = LuongCanBan + SoSanPham * 5000;
-}
-
-void NhanVienSX::Xuat()
-{
-    cout << "Nhan vien san xuat:" << endl;
-    NhanVien::Xuat();
-    cout << "Luong can ban: " << LuongCanBan << endl;
-    cout << "So san pham: " << SoSanPham << endl;
-}
-
-// ================= NHAN VIEN VAN PHONG =================
-
-NhanVienVP::NhanVienVP()
-{
-    SoNgayLamViec = 0;
-}
-
-NhanVienVP::~NhanVienVP()
-{
-}
-
-void NhanVienVP::Nhap()
-{
-    NhanVien::Nhap();
-
-    cout << "Nhap so ngay lam viec: ";
-    cin >> SoNgayLamViec;
-
-    TinhLuong();
-}
-
-void NhanVienVP::TinhLuong()
-{
-    Luong = SoNgayLamViec * 100000;
-}
-
-void NhanVienVP::Xuat()
-{
-    cout << "Nhan vien van phong:" << endl;
-    NhanVien::Xuat();
-    cout << "So ngay lam viec: " << SoNgayLamViec << endl;
 }
