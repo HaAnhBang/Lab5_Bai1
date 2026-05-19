6f041e8c2a630f96fa4985c7e4cfbac981628216
HaAnhBang <haanhbang4427@gmail.com>
2026-05-19 16:32:53 +0700

    Update NhanVien.h

--- NhanVien.h
+++ NhanVien.h
@@ -16,49 +16,13 @@ public:
     NhanVien();
     virtual ~NhanVien();
 
-    // Input: Nhap lan luot ho ten va ngay sinh
     virtual void Nhap();
-
-    // Output: xuat thong tin chung
     virtual void Xuat();
 
-    // moi lop con tu cai dat cach tinh luong rieng
     virtual void TinhLuong() = 0;
 
-    // return Luong
     long long GetLuong();
-
-    // return nam
     int GetNamSinh();
-};
-
-class NhanVienSX : public NhanVien
-{
-private:
-    int LuongCanBan;
-    int SoSanPham;
-
-public:
-    NhanVienSX();
-    ~NhanVienSX();
-
-    void Nhap();
-    void Xuat();
-    void TinhLuong();
-};
-
-class NhanVienVP : public NhanVien
-{
-private:
-    int SoNgayLamViec;
-
-public:
-    NhanVienVP();
-    ~NhanVienVP();
-
-    void Nhap();
-    void Xuat();
-    void TinhLuong();
 };
 
 #endif
