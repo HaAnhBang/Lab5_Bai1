7703c2d943ae3e7e218e64d4866be39b5f1f0ae3
HaAnhBang <haanhbang4427@gmail.com>
2026-05-19 16:31:54 +0700

    Update NhanVienVP.cpp

--- NhanVienVP.cpp
+++ NhanVienVP.cpp
@@ -1,1 +1,32 @@
 #include "NhanVienVP.h"
+
+NhanVienVP::NhanVienVP()
+{
+    SoNgayLamViec = 0;
+}
+
+NhanVienVP::~NhanVienVP()
+{
+}
+
+void NhanVienVP::Nhap()
+{
+    NhanVien::Nhap();
+
+    cout << "Nhap so ngay lam viec: ";
+    cin >> SoNgayLamViec;
+
+    TinhLuong();
+}
+
+void NhanVienVP::Xuat()
+{
+    NhanVien::Xuat();
+
+    cout << "So ngay lam viec: " << SoNgayLamViec << endl;
+}
+
+void NhanVienVP::TinhLuong()
+{
+    Luong = SoNgayLamViec * 100000;
+}
